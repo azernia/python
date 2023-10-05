58f07f49d4ee069c3473630eb600244801d6f9a6
rui <azernia@foxmail.com>
2023-10-05 22:02:04 +0800

    feat: file operate

--- basic/python_file.py
+++ basic/python_file.py
@@ -1,3 +1,17 @@
-file = open("../resources/a.txt", "r")
-file_content = file.read()
-print(file_content)
+def read_file(path):
+    file = open(path, "r", encoding="utf-8")
+    file_content = file.read()
+    print(file_content)
+    file.close()
+
+
+def write_file(path, content):
+    file = open(path, "w", encoding="utf-8")
+    file.write(content)
+    file.close()
+
+
+if __name__ == "__main__":
+    path = "../resources/a.txt"
+    read_file(path)
+    write_file(path=path, content="new content")
